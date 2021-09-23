ef68962ad714dd51cd507dc90304e9f2771dca86
Ian <egian93@gmail.com>
2021-09-23 13:39:26 -0500

    saves and renders events at loading

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Today's date in the following format: Jan 1st, 1999
+// Today's date in the following format: Jan 1st, 1999, 12:00:00 am
 var timer = setInterval(function(){
     var today = moment();
     $("#currentDay").text(today.format("MMM Do, YYYY, h:mm:ss a"));
     
 },1000);
-// const today = moment();
+
 
 const now = moment().format('HH')+':00'
 console.log(now)
@@ -13,14 +13,16 @@ console.log(now)
 
 var colLength = ['', '-9', '']
 var colColors = ['', 'bg-success','bg-primary']
-var dayHours = ['9:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00']
-
+var dayHours = ['9','10','11','12','13','14','15','16','17','18']
+var inputs = document.getElementById("my-form").elements;
+var inputByIndex = inputs[0];
+var inputEvent9 = inputs["Event9"];
 
 // Loop for row creation
 for (let j =0; j<dayHours.length; j++){
     var rowEl = $('<div>')
     rowEl.addClass('row')    
-    $('.container').append(rowEl)
+    $('#my-form').append(rowEl)
 
     // Loop for col creation 
     for(let i =0; i<3; i++){
@@ -28,6 +30,8 @@ for (let j =0; j<dayHours.length; j++){
             var colEl = $('<input>')
             // TODO Add atribute of id
             colEl.attr('id',dayHours[j])
+            colEl.attr('type','text')
+            colEl.attr('name','Event'+dayHours[j]);
             // TODO Add attr of placeholder
             colEl.attr('placeholder', 'New Event')
             colEl.attr('style', 'border: dashed 1px purple;border-radius: 5px;color: white;')
@@ -37,6 +41,7 @@ for (let j =0; j<dayHours.length; j++){
         } else if( i===2){
             var colEl = $('<button>')
             colEl.attr('class','save')
+            colEl.attr('type','submit')
             colEl.attr('style', 'border: none;color: white;border-radius: 5px;')
             colEl.addClass(('col')+colLength[i])
             colEl.text("💾 Save Event")
@@ -50,18 +55,18 @@ for (let j =0; j<dayHours.length; j++){
         }
         
         
-        // Validation for column of the hour
+        // Validation for the column of the hour
         if(i===0){
             colEl.addClass(('col')+colLength[i])            
             var colEl2 = $('<div>')
             colEl2.attr('style','text-align: end;position: relative;top: 1.9rem;')            
-            colEl2.text(dayHours[j])
+            colEl2.text(dayHours[j]+':00')
             colEl.append(colEl2)        
             
         } 
         
         // Validation for changing the color in the current hour      
-        if(i===1 && dayHours[j]===now){
+        if(i===1 && dayHours[j]+':00'===now){
             colEl.addClass('bg-warning')
             colEl.text("Current Hour")
             // colEl.addId('now')
@@ -78,25 +83,30 @@ for (let j =0; j<dayHours.length; j++){
     
     }
 }
-function saveLastEvent() {
+function saveLastEvent(event) {
+    var eventDetails = [];
+    // The loop is to iterate this function through out all the rows
     for(let i = 0;i<dayHours.length;i++){
 
-        var newEvent = $('#'+dayHours[i])
-        console.log('It is saving data')
-        // Save related form data as an object
-        var (eventDetails+dayHours[i]) = {
-          event: newEvent.value     
-        };
+        event.preventDefault();
+        // console.log('It is saving data');
+        eventDetails.push(inputs[i].value);
+        
         // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
-        localStorage.setItem("eventDetails"+dayHours[i], JSON.stringify(eventDetails+dayHours[i]));
     }
+    localStorage.setItem("eventDetails", JSON.stringify(eventDetails));
+    console.log(eventDetails);
 }
 function renderLastEvent() {
     // Use JSON.parse() to convert text to JavaScript object
     var lastEvent = JSON.parse(localStorage.getItem("eventDetails"));
+    console.log(lastEvent);
     // Check if data is returned, if not exit out of the function
-    if (lastGrade !== null) {
-    newEvent.text(lastEvent.event)
+    if (lastEvent !== null) {
+        for (let i =0; i<dayHours.length;i++){
+            inputs[i].value=lastEvent[i];
+        }
+    
     
     } else {
       return;
@@ -105,4 +115,4 @@ function renderLastEvent() {
 
 var saveButton = $('button')
 saveButton.on('click', saveLastEvent);
-
+renderLastEvent();
